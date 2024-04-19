c37b7197c81eaf6098896ed80fc092bcc5a42e0a
runner_bot <runner@runner.com>
2024-04-20 01:04:49 +0800

    Adding main code

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from pdf2docx import Converter
+from io import BytesIO
+
+def main():
+    st.write("hello world")
+
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+        cv = Converter(stream=bytes_data)
+        out_stream = BytesIO()
+        cv.convert(out_stream)
+        cv.close()
+        # Download the file
+        btn = st.download_button(
+            label="Download image",
+            data=out_stream.getvalue(),
+            file_name="sample.docx",
+            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        )
+
+main()
